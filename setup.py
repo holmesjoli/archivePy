a6413ea9138f7012e1afb374d3cf528df8bc7cf9
Joli Holmes <jh111@ad.rice.edu>
2019-04-11 13:22:46 -0500

    added setup information

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "gitToolsPy",
+    version = "0.0.1",
+    author = "Joli Holmes",
+    author_email = "holmesjoli@gmail.com",
+    description = "Git data archiving functions",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/holmesjoli/gitToolPy",
+    packages = setuptools.find_packages(),
+    install_requires = []
+)
 
