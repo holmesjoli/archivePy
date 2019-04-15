e6ad8cdf6e1d7769e1eee85d9f68d915f9aea150
Joli Holmes <jh111@ad.rice.edu>
2019-04-15 13:36:11 -0500

    added tests_create_archive function

--- dataArchivePy/tests/test_archive.py
+++ dataArchivePy/tests/test_archive.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from utilsPy.folder_structure import remove_dirs, create_dirs
+from utilsPy.folder_structure import remove_dirs, create_files
 from dataArchivePy.archive import archive_files, extract_archive, archive_folders
 
 class ArchiveTestClass(unittest.TestCase, archive_folders):
@@ -10,14 +10,17 @@ class ArchiveTestClass(unittest.TestCase, archive_folders):
  
         super(ArchiveTestClass, self).__init__(*args, **kwargs)
 
-        self.commit = "789hk4"
+        self.commit = "master_789hk4"
         self.output_pth = "./tests/test_archive/"
-        self.archive_fls = ["test_data1.csv", "test_data2.csv"]
+        self.archive_fls = ["./tests/test_data1.csv", "./tests/test_data2.csv"]
         self.fl = "test_data1.csv"
 
         self.af = archive_files(self.commit, self.output_pth, self.archive_fls)
         self.archive_dirs = self.af.archive_dirs
-        self.ea = extract_archive(self.commit, self.pth, self.fl)
+        self.current_dir = self.af.current_dir
+        self.archive_dir = self.af.archive_dir
+        self.zipped_fl = "{}.zip".format(self.commit)
+        # self.ea = extract_archive(self.commit, self.output_pth, self.fl)
 
         self.setUp()
 
@@ -29,8 +32,26 @@ class ArchiveTestClass(unittest.TestCase, archive_folders):
         
         self.af.create_archive_str()
         self.assertTrue(all([os.path.exists(d) for d in self.archive_dirs]))
+        remove_dirs(self.archive_dirs)
+
+    def test_move_to_current(self):
+        
+        create_files(self.archive_fls)
+        self.assertTrue(all([os.path.exists(d) for d in self.archive_fls]))
+        self.af.create_archive_str()
+        self.af.move_to_current()
+        self.assertTrue(all([os.path.exists(os.path.join(self.current_dir, os.path.basename(d))) for d in self.archive_fls]))
+
+    def test_create_archive(self):
+        create_files(self.archive_fls)
+        self.af.create_archive_str()
+        self.af.move_to_current()
+        self.af.create_archive()
+        self.assertTrue(os.path.exists(os.path.join(self.archive_dir, self.zipped_fl)))
+
         
 
-    def test_extract_archive(self):
 
-        self.assertTrue(hasattr(self.ea, "df"))
+    # # def test_extract_archive(self):
+
+    #     self.assertTrue(hasattr(self.ea, "df"))
