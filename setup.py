03c33e4f9186c22cf2513a95093880fc9136af05
Joli Holmes <jh111@ad.rice.edu>
2019-04-15 13:00:58 -0500

    updated setuptools with new name/repo

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "gitToolsPy",
+    name = "dataArchivePy",
     version = "0.0.1",
     author = "Joli Holmes",
     author_email = "holmesjoli@gmail.com",
     description = "Git data archiving functions",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/holmesjoli/gitToolPy",
+    url = "https://github.com/holmesjoli/dataArchivePy",
     packages = setuptools.find_packages(),
     install_requires = ["GitPython"]
 )
